d5dc1408878136fff9b01c5e3497289acdc63d30
Abdias Rueda <abdiasrueda507@gmail.com>
2025-09-16 19:43:58 -0500

    lab 2

---
+++ Laboratorio 2/Laboratorio2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    public class Program
+    { 
+        public static void Main()
+        {
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+     
+    public class MyClass
+    {
+        public static int Valor;
+    }
+
+}
