6af45d2212ceed2ae9ba53ee115acd2f392b720d
Abdias Rueda <abdiasrueda507@gmail.com>
2025-09-16 19:38:38 -0500

    Laboratorio 2

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Abdias";
+            client.LastName = "Rueda";
+            client.Age = 21;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+     public int Id { get; set; }
+     public string FirstName { get; set; }
+     public string LastName { get; set; }
+     public ushort Age { get; set; }
+
+     public string GetFullName()
+     {
+         return FirstName + " " + LastName;
+      }
+    }
+}
