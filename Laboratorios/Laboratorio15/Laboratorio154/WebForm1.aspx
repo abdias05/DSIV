46d45fb82fd1c5a4057762e7bac5c0a5d85f26e6
Abdias Rueda <abdiasrueda507@gmail.com>
2025-11-11 19:39:21 -0500

    Laboratorio 15

---
+++ Laboratorios/Laboratorio15/Laboratorio154/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Laboratorio154.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Introduzca 2 numeros:<br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+&nbsp; +<br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Enviar" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Laboratorios/Laboratorio15/Laboratorio154/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Laboratorio154
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        public object MessageBox { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int suma = 0;
+            int num1 = 0; 
+            int num2 = 0;
+       
+            try
+            {
+                 num1 = Convert.ToInt32(TextBox1.Text);
+
+            }
+            catch (FormatException)
+            {
+                string texto = "Error: numero invalido";
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "MessageBoxScript","window.alert('" + texto + "');",true);
+            }
+
+            catch (OverflowException)
+            {
+                String texto = "Error: el numero es demasiado grande";
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "MessageBoxScript", "window.alert('" + texto + "');", true);
+            }
+
+            try
+            {
+                 num2 = Convert.ToInt32(TextBox2.Text);
+
+            }
+            catch (FormatException)
+            {
+                string texto2 = "Error: numero invalido";
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "MessageBoxScript", "window.alert('" + texto2 + "');", true);
+            }
+
+            catch (OverflowException)
+            {
+                String texto2 = "Error: el numero es demasiado grande";
+                Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "MessageBoxScript", "window.alert('" + texto2 + "');", true);
+            }
+
+            suma= num1 + num2;
+
+            Label1.Text = "El resultado es = " + suma.ToString() + "";            
+        }
+    }
+}
