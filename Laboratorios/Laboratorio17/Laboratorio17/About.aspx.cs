eeb958b978941ee8e1f61191a3166e86e7ed31b1
Abdias Rueda <abdiasrueda507@gmail.com>
2025-11-16 15:25:30 -0500

    Laboratorio 17 terminado

---
+++ Laboratorios/Laboratorio17/Laboratorio17/About.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio17
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["conexionNorthwind"];
+            SqlConnection conexion = new SqlConnection(connString.ConnectionString);
+
+            using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";
+                conexion.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        if (reader.Read())
+                        {
+                            GridV.DataSource = reader;
+                            GridV.DataBind();
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Laboratorios/Laboratorio17/Laboratorio17/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Laboratorio17._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+        <div class="row">
+
+            <div>
+            <asp:GridView id="MyGridView" DataSourceID="MyDataSource1"
+AllowSorting="True" AllowPaging="True"
+DataKeyNames="ProductID"
+AutoGenerateEditButton="True"
+Runat="Server"/>
+
+<asp:SqlDataSource ID="MyDataSource1" runat="server"
+ConnectionString="data source=ADMINIS-E1FE361;initial catalog=northwind;persist security info=True;Integrated Security=SSPI;"
+ProviderName="System.Data.SqlClient"
+SelectCommand="SELECT ProductId, ProductName, UnitPrice From Products"
+UpdateCommand="Update Products Set [ProductName]=@ProductName, [UnitPrice]=@UnitPrice Where [ProductID]=@ProductID">
+</asp:SqlDataSource>
+      </div>
+               
+        </div>
+    
+
+</asp:Content>
