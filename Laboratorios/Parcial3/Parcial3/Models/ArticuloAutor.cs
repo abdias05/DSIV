9560fae32b5c74724f6f50b9ac6c093d605ec4e5
Abdias Rueda <abdiasrueda507@gmail.com>
2025-12-03 22:09:31 -0500

    commit

---
+++ Laboratorios/Parcial3/Parcial3/Models/ArticuloAutor.cs
@@ -0,0 +1,20 @@
+﻿/// Models/ArticuloAutor.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial3.Models
+{
+    [Table("AR_ArticulosAutores")]
+    public class ArticuloAutor
+    {
+        [Key, Column(Order = 0)]
+        public int ArticuloId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int AutorId { get; set; }
+
+        public virtual Articulo Articulo { get; set; }
+        public virtual Autor Autor { get; set; }
+    }
+
+}
---
+++ Laboratorios/Parcial3/Parcial3/Models/Autor.cs
@@ -0,0 +1,30 @@
+﻿
+// Models/Autor.cs
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial3.Models
+{
+    [Table("AR_Autores")]
+    public class Autor
+    {
+        [Key]
+        public int AutorId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Apellido { get; set; }
+
+        public virtual ICollection<ArticuloAutor> ArticulosAutores { get; set; }
+
+        public Autor()
+        {
+            ArticulosAutores = new HashSet<ArticuloAutor>();
+        }
+    }
+}
